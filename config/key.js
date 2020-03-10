4dc7fb5c01050bd458cd7e6e9d2593be191932f9
student <student@dblabs-Mac-mini.local>
2020-03-10 11:47:30 +0700

    get user profile

---
+++ config/key.js
@@ -0,0 +1,6 @@
+module.exports = {
+    google: {
+        clientID: "775721000882-dioi0k52p15sfog4qqibloqs65v16d2t.apps.googleusercontent.com",
+        clientSecret: "CnFmqYIs2_rI1n4Ipb_5JbMD"
+    }
+};
